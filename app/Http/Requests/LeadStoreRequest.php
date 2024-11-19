9bef89cb25dd49c13738bae4c90dda59402790fe
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-20 00:40:17 +0100

    added validation

--- app/Http/Requests/LeadStoreRequest.php
+++ app/Http/Requests/LeadStoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class LeadStoreRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class LeadStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+       return true;
     }
 
     /**
@@ -22,7 +23,9 @@ class LeadStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|min:3|max:80',
+            'email' =>'required|string|email|max:60',
+            'message'=>'required|string|min:10|max:2000',
         ];
     }
 }
