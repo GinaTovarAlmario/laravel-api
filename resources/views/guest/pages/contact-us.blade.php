6fe3da4c91abe6d29264e9a62992bc315268300c
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-20 00:11:36 +0100

    added basic structure to new page

---
+++ resources/views/guest/pages/contact-us.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="fw-bold">
+                    Contact us!
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <form action="" class="col-12">
+                
+            </form>
+        </div>
+    </div>
+</section>
+@endsection
