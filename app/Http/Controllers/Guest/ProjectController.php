b5aa3f43e6dd7a4a1f5b11b5355e77a41367d212
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-20 00:21:12 +0100

    defined method index and show for guest

--- app/Http/Controllers/Guest/ProjectController.php
+++ app/Http/Controllers/Guest/ProjectController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects = Project::orderBy('created_at', 'desc')->paginate(8);
+        return view('guest.projects.index',compact('posts'));
+    }
+    public function show(Project $project){
+        return view('guest.projects.show', compact('post'));
+    }
 }
