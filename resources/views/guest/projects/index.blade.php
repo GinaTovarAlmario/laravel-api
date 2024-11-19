eaa60708cdf9cf10d054e1b00c1e30285f7033ef
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-20 00:22:58 +0100

    fixed error typing

--- resources/views/guest/projects/index.blade.php
+++ resources/views/guest/projects/index.blade.php
@@ -29,7 +29,7 @@
                         </div>
                     @endforelse
                 <div>
-                    {{$project->links()}}
+                    {{$projects->links()}}
                 </div>
             </div>
         </div>
