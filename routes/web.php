4a7eb41e549922bede74a3c88fa6e96b70b7e91a
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-19 23:49:40 +0100

    added route guest

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
+use App\Http\Controllers\Guest\ProjectController as GuestProjectController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,17 +22,21 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+// ROTTE ADMIN
 Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
     Route::get('/projects', [AdminProjectController::class, 'index'])->name('projects.index');
     Route::post('/projects', [AdminProjectController::class, 'store'])->name('projects.store');
-
     Route::get('/projects/create', [AdminProjectController::class, 'create'])->name('projects.create');
     Route::delete("/projects/{project}", [AdminProjectController::class, "destroy"])->name("projects.delete");
     Route::put("/projects/{project}", [AdminProjectController::class, "update"])->name("projects.update");
     Route::get('/projects/{project}', [AdminProjectController::class, 'show'])->name('projects.show');
     Route::get("/projects/{project}/edit", [AdminProjectController::class, "edit"])->name("projects.edit");
-
+});
+// ROTTE GUEST
+Route::name('guest.')->group(function(){
+    Route::get('/', [GuestProjectController::class,'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('/projects', GuestProjectController::class)->only(['index','show']);
 
 });
