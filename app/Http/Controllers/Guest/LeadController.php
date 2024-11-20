597e5de8b75cc1c7822ef5f77f1f9d62fb748484
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-20 01:02:34 +0100

    finished store method

--- app/Http/Controllers/Guest/LeadController.php
+++ app/Http/Controllers/Guest/LeadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LeadStoreRequest;
+use App\Mail\NewContactForm;
 use App\Models\Lead;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
@@ -17,6 +18,7 @@ class LeadController extends Controller
     public function store(LeadStoreRequest $request){
         $data = $request->validated();
         $lead = Lead::create($data);
-        Mail::to('pippo.admin@gmail.com')->send();
+        Mail::to('pippo.admin@gmail.com')->send(new NewContactForm($lead));
+        return redirect()->route('guest.projects.index');
     }
 }
