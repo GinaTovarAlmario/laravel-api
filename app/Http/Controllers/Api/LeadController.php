f71f984a3c8f8188a6fc66e5ca114c8bad832b13
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-11-20 16:57:10 +0100

    added store that need some fixing first

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LeadStoreRequest;
+use App\Mail\NewContactForm;
+use App\Models\Lead;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
-    //
+    public function store(LeadStoreRequest $request){
+        $data = $request->validated();
+        $lead = Lead::create($data);
+        Mail::to('pippo.admin@gmail.com')->send(new NewContactForm($lead));
+    }
+
 }
